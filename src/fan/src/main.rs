a7c7c31e07846d9f9c20a9da5f21737330be9db0
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-12-07 20:40:29 +0100

    Replace constants with arguments

--- src/fan/src/main.rs
+++ src/fan/src/main.rs
@@ -8,28 +8,29 @@ use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 
-const DELIMITER: &'static str = ",";
-const NAME_INDEX: usize = 2;
-const SELECTION: &'static [usize] = &[0, 1, 2, 3, 4, 5];
-
 macro_rules! ok(($result:expr) => ($result.unwrap()));
 
 fn main() {
     let arguments = ok!(arguments::parse(env::args()));
+    let delimiter = arguments.get::<String>("delimiter").unwrap_or(",".to_string());
+    let group = ok!(arguments.get::<usize>("group"));
     let input = ok!(arguments.get::<String>("input"));
     let output = ok!(arguments.get::<String>("output"));
+    let selection = ok!(arguments.get::<String>("selection")).split(&delimiter)
+                                                             .map(|i| ok!(i.parse::<usize>()))
+                                                             .collect::<Vec<_>>();
     let output = Path::new(&output);
     let mut input = ok!(File::open(input));
     let mut decoder = ok!(GzDecoder::new(&mut input));
     let reader = BufReader::new(&mut decoder);
     for line in reader.lines() {
         let line = ok!(line);
-        let chunks = line.split(DELIMITER).collect::<Vec<_>>();
-        let output = output.join(chunks[NAME_INDEX]).with_extension("csv");
+        let chunks = line.split(&delimiter).collect::<Vec<_>>();
+        let output = output.join(chunks[group]).with_extension("csv");
         let mut output = ok!(OpenOptions::new().append(true).create(true).open(output));
-        for (i, &j) in SELECTION.iter().enumerate() {
+        for (i, &j) in selection.iter().enumerate() {
             if i > 0 {
-                ok!(output.write_all(DELIMITER.as_bytes()));
+                ok!(output.write_all(delimiter.as_bytes()));
             }
             ok!(output.write_all(chunks[j].as_bytes()));
         }
