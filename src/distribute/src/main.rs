bbc99c084436b40a7f1ce067e11eb47911226e9b
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-12-08 21:35:03 +0100

    Employ MD5 to have fewer files per folder

--- src/distribute/src/main.rs
+++ src/distribute/src/main.rs
@@ -1,10 +1,11 @@
 extern crate arguments;
 extern crate flate2;
+extern crate md5;
 
 use flate2::read::GzDecoder;
 
 use std::env;
-use std::fs::{File, OpenOptions};
+use std::fs::{self, File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 
@@ -26,7 +27,9 @@ fn main() {
     for line in reader.lines() {
         let line = ok!(line);
         let chunks = line.split(&delimiter).collect::<Vec<_>>();
-        let output = output.join(chunks[group]).with_extension("csv");
+        let directory = output.join(&digest(chunks[group])[..2]);
+        ok!(fs::create_dir_all(&directory));
+        let output = directory.join(chunks[group]).with_extension("csv");
         let mut output = ok!(OpenOptions::new().append(true).create(true).open(output));
         for (i, &j) in selection.iter().enumerate() {
             if i > 0 {
@@ -36,4 +39,12 @@ fn main() {
         }
         ok!(output.write_all("\n".as_bytes()));
     }
+}
+
+fn digest(data: &str) -> String {
+    let mut line = String::with_capacity(2 * 16);
+    for byte in md5::compute(data.as_bytes()).iter() {
+        line.push_str(&format!("{:02x}", byte));
+    }
+    line
 }
