322f1cce05a737b15d685c2b7eb31c071942ef74
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-01-13 10:54:20 +0100

    summarize: add annotation

--- bin/summarize.sh
+++ bin/summarize.sh
@@ -10,34 +10,24 @@ function execute() {
 }
 
 function job_events() {
-  sample_count_query="SELECT COUNT(*) FROM \`${table_name}\`;"
   job_count_query="SELECT COUNT(DISTINCT \`job ID\`) FROM \`${table_name}\`;"
   user_count_query="SELECT COUNT(DISTINCT \`user\`) FROM \`${table_name}\`;"
-  sample_count=$(execute "${1}" "${sample_count_query}")
+  sample_count_query="SELECT COUNT(*) FROM \`${table_name}\`;"
   job_count=$(execute "${1}" "${job_count_query}")
   user_count=$(execute "${1}" "${user_count_query}")
-  echo "${1}: ${sample_count} ${job_count} ${user_count}"
+  sample_count=$(execute "${1}" "${sample_count_query}")
+  echo "${1}: jobs ${job_count}, users ${user_count}, samples ${sample_count}"
 }
 
 function task_usage() {
-  sample_count_query="SELECT COUNT(*) FROM \`${table_name}\`;"
   job_count_query="SELECT COUNT(DISTINCT \`job ID\`) FROM \`${table_name}\`;"
   task_count_query="SELECT COUNT(*) FROM (SELECT COUNT(*) FROM \`${table_name}\` GROUP BY \`job ID\`, \`task index\`);"
-  sample_count=$(execute "${1}" "${sample_count_query}")
+  sample_count_query="SELECT COUNT(*) FROM \`${table_name}\`;"
   job_count=$(execute "${1}" "${job_count_query}")
   task_count=$(execute "${1}" "${task_count_query}")
-  echo "${1}: ${sample_count} ${job_count} ${task_count}"
+  sample_count=$(execute "${1}" "${sample_count_query}")
+  echo "${1}: jobs ${job_count}, tasks, ${task_count}, samples ${sample_count}"
 }
-
-case "${table_name}" in
-  'job_events')
-    ;;
-  'task_usage')
-    ;;
-  *)
-    echo 'Error: no table is given, or it is unknown.' && exit 1
-    ;;
-esac
 
 for part_path in $(find ${data_path} -name '*.sqlite3' | sort); do
   ${table_name} "${part_path}"
